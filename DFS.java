c9ebef4dd77b2b3ed0e4149fa105d2c7973e294d
Silas Agnew <agnewsilas@gmail.com>
2020-09-24 18:44:54 -0400

    Added automation to adjacency matrix generation

--- DFS.java
+++ DFS.java
@@ -70,6 +70,22 @@ public class DFS {
         }
     }
 
+    private static void printMatrix(int matrix[][]) {
+        System.out.print("   ");
+        for (int i = 0; i < 256; i++)
+        {
+            System.out.print(String.format("%3d ", i));
+        }
+        System.out.println();
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.print(String.format("%-3d", i));
+            for (int j : matrix[i]) {
+                System.out.print(String.format("%3d ", j));
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String arg[]) {
 
         // instantiate matrix
@@ -92,305 +108,29 @@ public class DFS {
         }
 
         // FIXME: manually add in the spots that should be '1' in adjacency matrix
-        adjacency_matrix[0][1] = 1;
-        adjacency_matrix[0][16] = 1;
-        adjacency_matrix[0][17] = 1;
-        adjacency_matrix[1][0] = 1;
-        adjacency_matrix[1][2] = 1;
-        adjacency_matrix[1][16] = 1;
-        adjacency_matrix[1][17] = 1;
-        adjacency_matrix[1][18] = 1;
-        adjacency_matrix[2][1] = 1;
-        adjacency_matrix[2][3] = 1;
-        adjacency_matrix[2][17] = 1;
-        adjacency_matrix[2][18] = 1;
-        adjacency_matrix[2][19] = 1;
-        adjacency_matrix[3][2] = 1;
-        adjacency_matrix[3][4] = 1;
-        adjacency_matrix[3][18] = 1;
-        adjacency_matrix[3][19] = 1;
-        adjacency_matrix[3][20] = 1;
-        adjacency_matrix[4][3] = 1;
-        adjacency_matrix[4][5] = 1;
-        adjacency_matrix[4][19] = 1;
-        adjacency_matrix[4][20] = 1;
-        adjacency_matrix[4][21] = 1;
-        adjacency_matrix[5][4] = 1;
-        adjacency_matrix[5][6] = 1;
-        adjacency_matrix[5][20] = 1;
-        adjacency_matrix[5][21] = 1;
-        adjacency_matrix[5][22] = 1;
-        adjacency_matrix[6][5] = 1;
-        adjacency_matrix[6][7] = 1;
-        adjacency_matrix[6][21] = 1;
-        adjacency_matrix[6][22] = 1;
-        adjacency_matrix[6][23] = 1;
-        adjacency_matrix[7][6] = 1;
-        adjacency_matrix[7][22] = 1;
-        adjacency_matrix[7][23] = 1;
-        adjacency_matrix[8][9] = 1;
-        adjacency_matrix[8][24] = 1;
-        adjacency_matrix[8][25] = 1;
-        adjacency_matrix[9][8] = 1;
-        adjacency_matrix[9][10] = 1;
-        adjacency_matrix[9][24] = 1;
-        adjacency_matrix[9][25] = 1;
-        adjacency_matrix[9][26] = 1;
-        adjacency_matrix[10][9] = 1;
-        adjacency_matrix[10][11] = 1;
-        adjacency_matrix[10][25] = 1;
-        adjacency_matrix[10][26] = 1;
-        adjacency_matrix[10][27] = 1;
-        adjacency_matrix[11][10] = 1;
-        adjacency_matrix[11][12] = 1;
-        adjacency_matrix[11][26] = 1;
-        adjacency_matrix[11][27] = 1;
-        adjacency_matrix[11][28] = 1;
-        adjacency_matrix[12][11] = 1;
-        adjacency_matrix[12][13] = 1;
-        adjacency_matrix[12][27] = 1;
-        adjacency_matrix[12][28] = 1;
-        adjacency_matrix[12][29] = 1;
-        adjacency_matrix[13][12] = 1;
-        adjacency_matrix[13][14] = 1;
-        adjacency_matrix[13][28] = 1;
-        adjacency_matrix[13][29] = 1;
-        adjacency_matrix[13][30] = 1;
-        adjacency_matrix[14][13] = 1;
-        adjacency_matrix[14][15] = 1;
-        adjacency_matrix[14][29] = 1;
-        adjacency_matrix[14][30] = 1;
-        adjacency_matrix[14][31] = 1;
-        adjacency_matrix[15][14] = 1;
-        adjacency_matrix[15][30] = 1;
-        adjacency_matrix[15][31] = 1;
-        adjacency_matrix[16][0] = 1;
-        adjacency_matrix[16][1] = 1;
-        adjacency_matrix[16][17] = 1;
-        adjacency_matrix[16][32] = 1;
-        adjacency_matrix[16][33] = 1;
-        adjacency_matrix[17][16] = 1;
-        adjacency_matrix[17][0] = 1;
-        adjacency_matrix[17][1] = 1;
-        adjacency_matrix[17][2] = 1;
-        adjacency_matrix[17][18] = 1;
-        adjacency_matrix[17][32] = 1;
-        adjacency_matrix[17][33] = 1;
-        adjacency_matrix[17][34] = 1;
-        adjacency_matrix[18][17] = 1;
-        adjacency_matrix[18][19] = 1;
-        adjacency_matrix[18][1] = 1;
-        adjacency_matrix[18][2] = 1;
-        adjacency_matrix[18][3] = 1;
-        adjacency_matrix[18][33] = 1;
-        adjacency_matrix[18][34] = 1;
-        adjacency_matrix[18][35] = 1;
-        adjacency_matrix[19][18] = 1;
-        adjacency_matrix[19][20] = 1;
-        adjacency_matrix[19][2] = 1;
-        adjacency_matrix[19][3] = 1;
-        adjacency_matrix[19][4] = 1;
-        adjacency_matrix[19][34] = 1;
-        adjacency_matrix[19][35] = 1;
-        adjacency_matrix[20][19] = 1;
-        adjacency_matrix[20][21] = 1;
-        adjacency_matrix[20][3] = 1;
-        adjacency_matrix[20][4] = 1;
-        adjacency_matrix[20][5] = 1;
-        adjacency_matrix[20][35] = 1;
-        adjacency_matrix[21][20] = 1;
-        adjacency_matrix[21][4] = 1;
-        adjacency_matrix[21][5] = 1;
-        adjacency_matrix[21][6] = 1;
-        adjacency_matrix[22][5] = 1;
-        adjacency_matrix[22][6] = 1;
-        adjacency_matrix[22][7] = 1;
-        adjacency_matrix[22][23] = 1;
-        adjacency_matrix[22][38] = 1;
-        adjacency_matrix[22][39] = 1;
-        adjacency_matrix[23][22] = 1;
-        adjacency_matrix[23][6] = 1;
-        adjacency_matrix[23][7] = 1;
-        adjacency_matrix[23][38] = 1;
-        adjacency_matrix[23][39] = 1;
-        adjacency_matrix[24][8] = 1;
-        adjacency_matrix[24][9] = 1;
-        adjacency_matrix[24][25] = 1;
-        adjacency_matrix[24][40] = 1;
-        adjacency_matrix[24][41] = 1;
-        adjacency_matrix[25][24] = 1;
-        adjacency_matrix[25][8] = 1;
-        adjacency_matrix[25][9] = 1;
-        adjacency_matrix[25][40] = 1;
-        adjacency_matrix[25][41] = 1;
-        adjacency_matrix[25][10] = 1;
-        adjacency_matrix[25][42] = 1;
-        adjacency_matrix[26][9] = 1;
-        adjacency_matrix[26][10] = 1;
-        adjacency_matrix[26][11] = 1;
-        adjacency_matrix[26][27] = 1;
-        adjacency_matrix[27][10] = 1;
-        adjacency_matrix[27][11] = 1;
-        adjacency_matrix[27][12] = 1;
-        adjacency_matrix[27][28] = 1;
-        adjacency_matrix[27][44] = 1;
-        adjacency_matrix[28][11] = 1;
-        adjacency_matrix[28][12] = 1;
-        adjacency_matrix[28][13] = 1;
-        adjacency_matrix[28][29] = 1;
-        adjacency_matrix[28][44] = 1;
-        adjacency_matrix[28][45] = 1;
-        adjacency_matrix[29][12] = 1;
-        adjacency_matrix[29][13] = 1;
-        adjacency_matrix[29][14] = 1;
-        adjacency_matrix[29][30] = 1;
-        adjacency_matrix[29][44] = 1;
-        adjacency_matrix[29][45] = 1;
-        adjacency_matrix[29][46] = 1;
-        adjacency_matrix[30][13] = 1;
-        adjacency_matrix[30][14] = 1;
-        adjacency_matrix[30][15] = 1;
-        adjacency_matrix[30][31] = 1;
-        adjacency_matrix[30][45] = 1;
-        adjacency_matrix[30][46] = 1;
-        adjacency_matrix[30][47] = 1;
-        adjacency_matrix[31][14] = 1;
-        adjacency_matrix[31][15] = 1;
-        adjacency_matrix[31][46] = 1;
-        adjacency_matrix[31][47] = 1;
-        adjacency_matrix[32][16] = 1;
-        adjacency_matrix[32][17] = 1;
-        adjacency_matrix[32][33] = 1;
-        adjacency_matrix[32][48] = 1;
-        adjacency_matrix[32][49] = 1;
-        adjacency_matrix[33][16] = 1;
-        adjacency_matrix[33][17] = 1;
-        adjacency_matrix[33][18] = 1;
-        adjacency_matrix[33][32] = 1;
-        adjacency_matrix[33][34] = 1;
-        adjacency_matrix[33][48] = 1;
-        adjacency_matrix[33][49] = 1;
-        adjacency_matrix[33][50] = 1;
-        adjacency_matrix[34][17] = 1;
-        adjacency_matrix[34][18] = 1;
-        adjacency_matrix[34][19] = 1;
-        adjacency_matrix[34][33] = 1;
-        adjacency_matrix[34][35] = 1;
-        adjacency_matrix[34][49] = 1;
-        adjacency_matrix[34][50] = 1;
-        adjacency_matrix[34][51] = 1;
-        adjacency_matrix[35][18] = 1;
-        adjacency_matrix[35][19] = 1;
-        adjacency_matrix[35][20] = 1;
-        adjacency_matrix[35][34] = 1;
-        adjacency_matrix[35][50] = 1;
-        adjacency_matrix[35][51] = 1;
-        adjacency_matrix[36][37] = 1;
-        adjacency_matrix[36][52] = 1;
-        adjacency_matrix[36][53] = 1;
-        adjacency_matrix[37][36] = 1;
-        adjacency_matrix[37][38] = 1;
-        adjacency_matrix[37][22] = 1;
-        adjacency_matrix[37][52] = 1;
-        adjacency_matrix[37][53] = 1;
-        adjacency_matrix[37][54] = 1;
-        adjacency_matrix[38][37] = 1;
-        adjacency_matrix[38][39] = 1;
-        adjacency_matrix[38][22] = 1;
-        adjacency_matrix[38][23] = 1;
-        adjacency_matrix[38][53] = 1;
-        adjacency_matrix[38][54] = 1;
-        adjacency_matrix[38][55] = 1;
-        adjacency_matrix[39][22] = 1;
-        adjacency_matrix[39][23] = 1;
-        adjacency_matrix[39][38] = 1;
-        adjacency_matrix[39][54] = 1;
-        adjacency_matrix[39][55] = 1;
-        adjacency_matrix[39][56] = 1;
-        adjacency_matrix[40][24] = 1;
-        adjacency_matrix[40][25] = 1;
-        adjacency_matrix[40][41] = 1;
-        adjacency_matrix[40][55] = 1;
-        adjacency_matrix[40][56] = 1;
-        adjacency_matrix[40][57] = 1;
-        adjacency_matrix[41][40] = 1;
-        adjacency_matrix[41][24] = 1;
-        adjacency_matrix[41][25] = 1;
-        adjacency_matrix[41][42] = 1;
-        adjacency_matrix[41][56] = 1;
-        adjacency_matrix[41][57] = 1;
-        adjacency_matrix[41][58] = 1;
-        adjacency_matrix[42][41] = 1;
-        adjacency_matrix[42][25] = 1;
-        adjacency_matrix[42][42] = 1;
-        adjacency_matrix[42][57] = 1;
-        adjacency_matrix[42][58] = 1;
-        adjacency_matrix[42][59] = 1;
-        adjacency_matrix[43][42] = 1;
-        adjacency_matrix[43][58] = 1;
-        adjacency_matrix[43][59] = 1;
-        adjacency_matrix[44][27] = 1;
-        adjacency_matrix[44][28] = 1;
-        adjacency_matrix[44][29] = 1;
-        adjacency_matrix[44][45] = 1;
-        adjacency_matrix[44][60] = 1;
-        adjacency_matrix[44][61] = 1;
-        adjacency_matrix[45][28] = 1;
-        adjacency_matrix[45][29] = 1;
-        adjacency_matrix[45][30] = 1;
-        adjacency_matrix[45][44] = 1;
-        adjacency_matrix[45][46] = 1;
-        adjacency_matrix[45][60] = 1;
-        adjacency_matrix[45][61] = 1;
-        adjacency_matrix[45][62] = 1;
-        adjacency_matrix[46][29] = 1;
-        adjacency_matrix[46][30] = 1;
-        adjacency_matrix[46][31] = 1;
-        adjacency_matrix[46][45] = 1;
-        adjacency_matrix[46][47] = 1;
-        adjacency_matrix[46][61] = 1;
-        adjacency_matrix[46][62] = 1;
-        adjacency_matrix[46][63] = 1;
-        adjacency_matrix[47][30] = 1;
-        adjacency_matrix[47][31] = 1;
-        adjacency_matrix[47][46] = 1;
-        adjacency_matrix[47][62] = 1;
-        adjacency_matrix[47][63] = 1;
-        adjacency_matrix[48][32] = 1;
-        adjacency_matrix[48][33] = 1;
-        adjacency_matrix[48][49] = 1;
-        adjacency_matrix[48][64] = 1;
-        adjacency_matrix[48][65] = 1;
-        adjacency_matrix[49][32] = 1;
-        adjacency_matrix[49][33] = 1;
-        adjacency_matrix[49][34] = 1;
-        adjacency_matrix[49][48] = 1;
-        adjacency_matrix[49][50] = 1;
-        adjacency_matrix[49][64] = 1;
-        adjacency_matrix[49][65] = 1;
-        adjacency_matrix[50][33] = 1;
-        adjacency_matrix[50][34] = 1;
-        adjacency_matrix[50][35] = 1;
-        adjacency_matrix[50][49] = 1;
-        adjacency_matrix[50][51] = 1;
-        adjacency_matrix[50][65] = 1;
-        adjacency_matrix[51][34] = 1;
-        adjacency_matrix[51][35] = 1;
-        adjacency_matrix[51][50] = 1;
-        adjacency_matrix[52][36] = 1;
-        adjacency_matrix[52][37] = 1;
-        adjacency_matrix[52][53] = 1;
-        adjacency_matrix[52][68] = 1;
-        adjacency_matrix[52][69] = 1;
-        adjacency_matrix[53][36] = 1;
-        adjacency_matrix[53][37] = 1;
-        adjacency_matrix[53][38] = 1;
-        adjacency_matrix[53][52] = 1;
-        adjacency_matrix[53][54] = 1;
-        adjacency_matrix[53][68] = 1;
-        adjacency_matrix[53][69] = 1;
-        
+        // The stupid complicated for loop parameters are just keeping the index in bounds on the edges
+        for (int i = 0; i < adjacency_matrix.length; i++) {
+            if (i > 15)
+                for (int up = (i - 1) % 16 == 15 ? i - 16 : i - 17;
+                     up < (i - 14 > 255 ? 255 : i - 14);
+                     up++)
+                    adjacency_matrix[i][up] = 1;
 
+
+            for (int mid = (i + 15) % 16 == 15 ? i : i - 1;
+                 mid < (i + 2 > 255 ? 255 : i + 2);
+                 mid++)
+                adjacency_matrix[i][mid] = 1;
+
+            if (i < 240)
+                for (int bot = (i + 15) % 16 == 15 ? i + 16 : i + 15;
+                     bot < (i + 18 > 255 ? 255 : i + 18);
+                     bot++)
+                    adjacency_matrix[i][bot] = 1;
+        }
+
+        // Display the matrix for testing.
+        printMatrix(adjacency_matrix);
 
         // Get starting point from the user
         Scanner scan = new Scanner(System.in);
